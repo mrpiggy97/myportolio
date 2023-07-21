53056ac83748b43f7a73e544a29316ca8a1d7246
mrpiggy97 <fabyjesusrivas10@gmail.com>
2023-07-21 04:47:23 +0000

    added new style to component

--- src/Project.tsx
+++ src/Project.tsx
@@ -4,37 +4,37 @@ export type projectProps = {
     name : string,
     gifURI : string | null,
     description : string,
-    stack : string[],
+    stack : string,
     link : string | null,
     github : string,
+    frontend : boolean
 }
 
 export default function Project(props : projectProps) : JSX.Element{
+    const subClass : string = props.frontend === true ? "frontend" : ""
+    const finalClass = `project3 ${subClass}`
     return(
         <div className="project-container">
             <div className="project1">
             </div>
             <div className="project2">
             </div>
-            <div className="project3">
+            <div className={finalClass}>
                 <div className="name">
                     <span>{props.name}</span>
                 </div>
-                <div className="github">
-                    <span>repo: {props.github}</span>
-                </div>
-                <div className="link">
-                    {props.link ? <span>link: {props.link}</span> : null}
+                <div className="links">
+                    <a className="link" target="_blank" href={props.github}>click here to see github repo</a>
+                    {props.link ? <a className="link" target="_blank" href={props.link}>click here to see site</a> : null}
                 </div>
                 <div className="stack">
-                    <span>tech stack: {props.stack}</span>
+                    <span>stack: {props.stack}</span>
                 </div>
                 <div className="description">
-                    <span>{props.description}</span>
+                    <span>{props.description}.</span>
                 </div>
                 <div className="preview">
-                    <span>preview</span>
-                    {props.gifURI ? <img src={props.gifURI} height={"100%"} width={"50%"} /> : null}
+                    {props.gifURI ? <img src={props.gifURI} height={"100%"} width={"95%"} /> : null}
                 </div>
             </div>
         </div>
