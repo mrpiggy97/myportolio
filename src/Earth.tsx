868e26ea95947f7532809df689948bd5eab4c2c8
mrpiggy97 <fabyjesusrivas10@gmail.com>
2023-07-24 17:15:21 +0000

    added mobile styles

--- src/Earth.tsx
+++ src/Earth.tsx
@@ -1,17 +1,19 @@
 import { useLoader, useFrame } from "@react-three/fiber"
-import { Mesh, TextureLoader } from "three"
+import { Mesh, TextureLoader, Vector3 } from "three"
 import earthTexture from "./assets/dayearth10k.jpg"
-import { useRef } from "react"
+import { useEffect, useRef } from "react"
 
 export default function Earth() : JSX.Element{
     const earthMap = useLoader(TextureLoader, earthTexture)
     const earthRef = useRef<Mesh>(null!)
+    const pos : number[] = window.innerWidth > 700 ? [1.5,1,0] : [0,0,0]
+    const positioning : Vector3 = new Vector3(...pos)
     useFrame(({clock}) => {
         const a = clock.getElapsedTime()/2
         earthRef.current.rotation.y = a*(-1)
     })
     return(
-        <mesh scale={2} position={[-1.5,1,0]} ref={earthRef}>
+        <mesh scale={2} position={positioning} ref={earthRef}>
             <ambientLight intensity={0.5} />
             <pointLight position={[-10, 0, 0]} intensity={0.5} />
             <sphereGeometry attach="geometry"/>
