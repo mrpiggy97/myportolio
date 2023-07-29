4b68493419b13ed709ed76eee0a1823d78c06204
mrpiggy97 <fabyjesusrivas10@gmail.com>
2023-07-29 18:47:20 +0000

    finished spacing objects

--- src/Earth.tsx
+++ src/Earth.tsx
@@ -6,14 +6,15 @@ import { useRef } from "react"
 export default function Earth() : JSX.Element{
     const earthMap = useLoader(TextureLoader, earthTexture)
     const earthRef = useRef<Mesh>(null!)
-    const pos : number[] = window.innerWidth > 700 ? [-1,1,0] : [0,0,0]
+    const pos : number[] = window.innerWidth > 700 ? [-1,1,0] : [0,-0.7,0]
+    const scale = window.innerWidth > 700 ? 2 : 2.5
     const positioning : Vector3 = new Vector3(...pos)
     useFrame(({clock}) => {
         const a = clock.getElapsedTime()/2
         earthRef.current.rotation.y = a*(-1)
     })
     return(
-        <mesh scale={2} position={positioning} ref={earthRef}>
+        <mesh scale={scale} position={positioning} ref={earthRef}>
             <ambientLight intensity={0.5} />
             <pointLight position={[-10, 0, 0]} intensity={0.5} />
             <sphereGeometry attach="geometry"/>
