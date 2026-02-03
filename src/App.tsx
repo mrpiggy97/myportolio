e37eeb62e4d627e2523b46fda0d7643299d7cb71
mrpiggy97 <fabyjesusrivas10@gmail.com>
2026-02-03 22:08:25 +0000

    Adds project directory, removes unsued images

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,50 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAngular, faConnectdevelop, faDocker, faGithub, faGitlab, faGolang, faJs, faLinkedin, faPython, faReact } from '@fortawesome/free-brands-svg-icons'
+import { faAngular, faDocker, faGithub, faGitlab, faGolang, faJs, faLinkedin, faPython, faReact } from '@fortawesome/free-brands-svg-icons'
 import './App.css'
-import { faCode, faDatabase, faH, faHandshake, faHSquare, faLink, faNetworkWired, faW } from '@fortawesome/free-solid-svg-icons'
+import { faArrowDown, faArrowRight, faCode, faDatabase, faHandshake, faHSquare, faList,faSearch} from '@fortawesome/free-solid-svg-icons'
+import { useState } from 'react'
+
+function ShowProjects() : JSX.Element{
+  const [showProjects, setShowProjects] = useState(true)
+  const toggleShowProjects = () => {
+    setShowProjects(!showProjects)
+  }
+  return (
+    <div className='projects-directories'>
+      <p>
+        <FontAwesomeIcon icon={faHandshake}/> Home
+      </p>
+      <p onClick={toggleShowProjects}>
+        {showProjects ?  <FontAwesomeIcon icon={faArrowDown}/> : <FontAwesomeIcon icon={faArrowRight}/>}
+        <FontAwesomeIcon icon={faList}/> Projects
+      </p>
+      {
+        showProjects
+        ? <blockquote>
+            <p>
+              <FontAwesomeIcon icon={faCode}/> devhttp
+            </p>
+            <p>
+              <FontAwesomeIcon icon={faCode}/> space-simulation
+            </p>
+            <p>
+              <FontAwesomeIcon icon={faCode}/> piggyhttp
+            </p>
+            <p>
+              <FontAwesomeIcon icon={faCode}/> pathfinder
+            </p>
+          </blockquote>
+        : null
+      }
+    </div>
+  )
+}
+
 function App() : JSX.Element {
+  const [srcExpanded, setSrcExpanded] = useState(false)
+  const toggleSrc = () => {
+    setSrcExpanded(!srcExpanded)
+  }
   return(
     <div id='app'>
 
@@ -30,7 +72,20 @@ function App() : JSX.Element {
             </a>
         </div>
         <div id='project-navigation'>
-
+          <div className='explorer'>
+            <p>EXPLORER <FontAwesomeIcon icon={faSearch}/></p>
+          </div>
+          <div className='navigation-header'>
+            <p>MY PROJECTS AND PRESENTATION</p>
+          </div>
+          <div className='projects'>
+            <div className='projects-source' onClick={toggleSrc}>
+                  {srcExpanded ? <FontAwesomeIcon icon={faArrowDown}/> : <FontAwesomeIcon icon={faArrowRight}/>}
+                  <FontAwesomeIcon icon={faList}/>
+                  <span>src</span>
+            </div>
+            {srcExpanded ? <ShowProjects/> : null}
+          </div>
         </div>
       </div>
 
