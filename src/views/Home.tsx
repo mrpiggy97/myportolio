bf59b72c29353fe456fe917702fdd6e9aecf8582
mrpiggy97 <fabyjesusrivas10@gmail.com>
2026-02-04 02:47:15 +0000

    Adds animation in matrix style

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,8 +1,41 @@
+import "./Home.css"
+import { useEffect, useState } from "react"
+
 export default function Home() : JSX.Element{
+    const [displayText, setDisplayText] = useState("Hello, I am Fabian")
+
+    useEffect(() => {
+        const japaneseChars = "アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヲン01"
+        const text = "Hello, I am Fabian"
+        
+        const startAnimation = () => {
+            let iteration = 0
+            const interval = setInterval(() => {
+                setDisplayText(
+                    text
+                        .split("")
+                        .map((char, index) => {
+                            if (char === " ") return " "
+                            if (index < iteration) return char
+                            return japaneseChars[Math.floor(Math.random() * japaneseChars.length)]
+                        })
+                        .join("")
+                )
+                if (iteration >= text.length) {
+                    clearInterval(interval)
+                    setTimeout(startAnimation, 5000)
+                }
+                iteration += 1 / 3
+            }, 30)
+        }
+        
+        startAnimation()
+    }, [])
+
     return(
         <div id="home">
             <div id="greeting-box">
-                <h1>Hello, I am Fabian</h1>
+                <h1>{displayText}</h1>
             </div>
         </div>
     )
